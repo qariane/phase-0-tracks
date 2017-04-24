637a8c2d54844f9f75854143529aaa28fa1f80d0
Ariane Quenum <quenumariane@gmail.com>
2017-04-23 21:19:44 -0500

    Create encrypt/decrypt

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,15 +11,8 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 
 order= gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
+
 enrollment= gets.chomp
 
-wolves_like_sunshine = true 
-
-wolves_like_garlic = true
-
-vampires_like_sunchine = false
-
-vampires_like_garlic = false
-
-
+if input == 
 
