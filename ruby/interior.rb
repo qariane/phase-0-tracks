8443a58cd576555d549df39969c8e487f32ee07a
Ariane Quenum <quenumariane@gmail.com>
2017-04-27 05:18:32 -0500

    Practice

---
+++ ruby/interior.rb
@@ -0,0 +1,39 @@
+interior = {
+
+	  :Name => "Ariane",
+
+	  :Address => "7064 chicago",
+
+	  "Email" => "qgen@gmail.com",
+
+	  "Phone" => "312-345-1234",
+
+	  "Fave shade of blue" => "bue",
+}
+
+
+p interior
+
+interior ["Name"] = "Genny" 
+
+p interior ["Name"]
+
+interior["good"] = 500
+
+interior["th"] = "tomor"
+
+p interior 
+ 
+  interior [:Name] + interior [:adress]
+
+  p interior 
+
+  interior [:Name] + interior [:adress]
+
+p interior 
+
+interior [:Name] + interior [:adress]
+
+
+p interior 
+
