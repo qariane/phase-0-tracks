2f321bc0490a0ee7c46f8fe17a41bd363fd24ac8
Ariane Quenum <quenumariane@gmail.com>
2017-04-29 12:23:01 -0500

    Add content

---
+++ ruby/iteration.rb
@@ -0,0 +1,16 @@
+
+
+
+def  week_end 
+
+	puts "I like week end because i don't have class"
+
+	yield 
+    yield
+    yield
+end
+
+week_end { puts "i can rest the whole day"}
+
+
+puts "I also have time to go to the gym"
