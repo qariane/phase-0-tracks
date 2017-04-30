af7262ac9d12e7162bba66d715c7eeee3cf00ee8
Ariane Quenum <quenumariane@gmail.com>
2017-04-29 22:35:49 -0500

    Add content

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -60,13 +60,15 @@ Hollywood_actors.each do |actors, moovie_name|
 
    a.delete_if {|x| x < 5 }
 
-  # A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+ # A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
     a = [1,2,3,4,5,6,7,8,9]
     a.select {|x| x < 5 } 
 
-   # A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+ # A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
 
-   
-
-  
+    a = [1,2,3,4,5,6,7,8,9]
+    a.select {|x| x.odd? }
+ # A method that will remove items from a data structure until the condition in the block evaluates to false, then stops (you may not find a perfectly working option for the hash, and that's okay).
+   a = [1,2,3,4,5,6,7,8,9]
+   a.take_while { |i| i < 5 }
                                                                                                    
