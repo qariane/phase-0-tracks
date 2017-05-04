c74ac348c409c2899f8df378d6468d38742db51b
Ariane Quenum <quenumariane@gmail.com>
2017-05-04 17:37:08 -0500

    Add content to pseudocode

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -10,4 +10,10 @@ class Calculator
   def multiply(x,y)
     x * y
   end
+
+  def divide(x,y)
+  	x / y 
+  end
+  
 end
+
---
+++ ruby/gps2_2.rb
@@ -0,0 +1,31 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+# create a list of items and quantities in a hash
+# separate string of items using space
+
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: push item and quantity in the hash 
+# output: show list with new items and quantities
+
+# Method to remove an item from the list
+# input:hash and item 
+# steps:find the item in the hash and remote
+# output:show list without the item removed
+
+# Method to update the quantity of an item
+# input:hash, item, quantity
+# steps:find the item in the hash and update the quantity
+# output:show list with new quantity
+
+# Method to print a list and make it look pretty
+# input:hash
+# steps:iterate through hash to print items and quantity
+# output:a list of items and quantity on separate line
