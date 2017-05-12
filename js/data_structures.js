fa0c0ba4f1cf9e28009c033916e83d33b3e95b7e
Ariane Quenum <quenumariane@gmail.com>
2017-05-12 11:37:41 -0500

    Create files and add arrays

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+
+//declaring arrays 
+
+var colors = ["blue","brown_caramel", "green_with_sparkles", "white"]
+var names = ["Ed", "Jack", "Phillly", "Chris"]
+//Add a color to the colors and names arrays
+colors.push("black");
+console.log(colors);
+names.push("Bob");
+console.log(names);
