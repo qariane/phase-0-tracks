7fef08126954fdeee0097a2793e67d3b5fc5599a
Ariane Quenum <quenumariane@gmail.com>
2017-05-14 03:33:29 -0500

    Update

--- js/practice.js
+++ js/practice.js
@@ -1,23 +1,2 @@
 
-  function random_test(num) {
-    var word =[];  
-    for(var j=0; j<num; j++) {  
-     
-     var alphabet = "abcdefghijklmnopqrstuvwxyz";
-     var num ="";
-     
-     for (var i =0; i < Math.ceil(Math.random()*3); i++) {
-       new_string += (alphabet[Math.round(Math.random() * 10)]);
-     }
-     
-     word.push(new_string);
-   }
-   return word;
- }
-
-
-  for (var i =0; i<10; i++) {
-   var random_strings = random_test(Math.ceil(Math.random()*10));
-   console.log(random_strings);
-   //console.log(longest_word(random_strings));
- }
+  
