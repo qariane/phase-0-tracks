38b69bfe56a1d21ad43cb72eb29ccc25fcacda66
Ariane Quenum <quenumariane@gmail.com>
2017-05-15 04:16:57 -0500

    Update

--- js/algos.js
+++ js/algos.js
@@ -40,8 +40,22 @@ function key_value(first_function, second_function) {
 //Release 2
 
 
+function random(input)
+ {
+     
+     var alphabet = "abcdefghijklmnopqrstuvwxyz";
+     
+     var word = " ";
+    
+     for (var i = 0; i < input; i++ )
 
-
+         word += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    
+     return word;
+    
+ }
+ 
+ console.log(random(6));
 
 
 
--- js/practice.js
+++ js/practice.js
@@ -1,2 +1,21 @@
 
-  
+
+
+
+
+function random(input)
+ {
+     
+     var alphabet = "abcdefghijklmnopqrstuvwxyz";
+     
+     var word = " ";
+    
+     for (var i = 0; i < input; i++ )
+
+         word += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    
+     return word;
+    
+ }
+ 
+ console.log(random(6));
