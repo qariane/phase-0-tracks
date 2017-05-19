1ccee1b6b44207d2cdf79e37be8aceb5017e50d6
Ariane Quenum <quenumariane@gmail.com>
2017-05-19 11:06:50 -0500

    Add content for release 1

--- ruby/problem_solving_methods_skippers.rb
+++ ruby/problem_solving_methods_skippers.rb
@@ -18,5 +18,27 @@ def search_array(arr, int)
     return position
 end
 
-arr = [42, 89, 23, 1]
-p search_array(arr, 1)
+#arr = [42, 89, 23, 1]
+#p search_array(arr, 1)
+
+def fibona(length)
+	index = 0 
+	fibona_array = Array.new(length)
+
+while index < fibona_array.length
+	
+	if index <= 1
+
+		fibona_array[index] = index
+	else 
+
+		fibona_array[index] = (fibona_array[index-2] + fibona_array[index-1])
+    end 
+
+    index += 1
+end
+
+   return fibona_array
+end
+
+#p fibona(100)
