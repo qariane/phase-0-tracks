f6d6cd0ac8b7b165d8befe58f43121c6b18d2109
Ariane Quenum <quenumariane@gmail.com>
2017-05-24 09:25:01 -0500

    Update

---
+++ ruby/puppy_skippers.rb
@@ -0,0 +1,19 @@
+
+
+
+#SPECIES 
+#Canis lupus familiaris
+
+#CHARACTERISTICS 
+#large canine teeth
+#long tail
+#worldwide distribution in close association with humans
+#Name: varies
+#Color: varies
+#long muzzle
+
+#BEHAVIOR 
+#Hunt 
+#Herd
+#pull 
+#protect
