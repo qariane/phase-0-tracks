aab8cd5d743297362e1ee245803a30261b64e4e8
Ariane Quenum <quenumariane@gmail.com>
2017-05-31 10:11:15 -0500

    Finish release 0

---
+++ js/data_structures_skippers.js
@@ -0,0 +1,17 @@
+color = ["purple", "iridescent blue", "black ash", "raspberry ombre"]
+name =  ["Ed", "John", "Peter", "Alex"]
+
+
+
+
+
+
+
+
+
+
+
+color.push("white");
+name.push("Steve");
+console.log(color);
+console.log(name);
