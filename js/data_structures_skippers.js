b61fddf090fa1790ff278af0ee3bafe9004da635
Ariane Quenum <quenumariane@gmail.com>
2017-06-01 17:16:17 -0500

    finish release 1

--- js/data_structures_skippers.js
+++ js/data_structures_skippers.js
@@ -1,8 +1,18 @@
-color = ["purple", "iridescent blue", "black ash", "raspberry ombre"]
-name =  ["Ed", "John", "Peter", "Alex"]
 
+//Release 0 
+var color = ["purple", "iridescent blue", "black ash", "raspberry ombre"]
+var name =  ["Ed", "John", "Peter", "Alex"]
+var hash = {}
 
+function namecolors(name, color){
+ for(var i = 0; i < name.length; i++) {
+ 	hash[name[i]] = color[i];
+ }; 
+ return hash;
+};
 
+namecolors(name,color);
+console.log(hash);
 
 
 
