dec16c6c41a33071c0f54b69ee13ac52535dc7bc
Ariane Quenum <quenumariane@gmail.com>
2017-06-01 17:35:10 -0500

    Finish release 2

--- js/data_structures_skippers.js
+++ js/data_structures_skippers.js
@@ -4,24 +4,37 @@ var color = ["purple", "iridescent blue", "black ash", "raspberry ombre"]
 var name =  ["Ed", "John", "Peter", "Alex"]
 var hash = {}
 
-function namecolors(name, color){
- for(var i = 0; i < name.length; i++) {
- 	hash[name[i]] = color[i];
- }; 
- return hash;
-};
-
-namecolors(name,color);
-console.log(hash);
-
-
-
-
-
-
-
-
 color.push("white");
 name.push("Steve");
 console.log(color);
 console.log(name);
+//Release 1
+function Namecolors(name, color){
+ for(var i = 0; i < name.length; i++) {
+ 	hash[name[i]] = color[i];
+ }; 
+ return hash;
+ namecolors(name,color);
+console.log(hash);
+
+};
+//Release 2 
+function Car(name, numberofdoor, speed) {
+	console.log("our new car:", this);
+
+this.name = name;
+this.numberofdoor = numberofdoor;
+this.speed = speed;
+console.log('your car is '  + this.name);
+};
+var car = new Car('lebarin', 2, 'fast'); 
+
+
+
+
+
+
+
+
+
+
